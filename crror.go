933831a15680b2f20f3cb46bc495e22f48e0b656
Takashi Oguma <bear.mini@gmail.com>
2014-05-21 19:43:53 +0900

    update patterns and fix formatting

--- crror.go
+++ crror.go
@@ -5,49 +5,58 @@ import (
 	"flag"
 	"fmt"
 	//  "github.com/andrew-d/go-termutil"
-	//  "io"
 	"log"
 	"os"
-    "strings"
+	"regexp"
+	"strings"
 )
 
 var (
 	input *bufio.Reader
+	re1   = regexp.MustCompile("make(\\[[0-9]+\\])?:")
 )
 
 const (
-    bold = "\033[1m"
-    reset = "\033[0m"
+	bold  = "\033[1m"
+	reset = "\033[0m"
 
-    red = "\033[31m"
-    green = "\033[32m"
-    yellow = "\033[33m"
-    blue = "\033[34m"
-    magenta = "\033[35m"
-    cyan = "\033[36m"
+	red     = "\033[31m"
+	green   = "\033[32m"
+	yellow  = "\033[33m"
+	blue    = "\033[34m"
+	magenta = "\033[35m"
+	cyan    = "\033[36m"
 )
 
+func first(args ...interface{}) interface{} {
+	return args[0]
+}
+
 func Colorize(line string) string {
-    tokens := strings.Split(line, " ")
-    if cap(tokens) < 2 {
-        return line
-    }
+	tokens := strings.Split(line, " ")
+	if cap(tokens) < 2 {
+		return line
+	}
 
-    cline := line
-    switch {
-        case (cap(tokens) > 2) && (tokens[1] == "undefined"):
-            cline = bold + tokens[0] + " " + red + strings.Join(tokens[1:], " ") + reset
-        case (cap(tokens) > 2) && (tokens[1] == "error:"):
-            cline = bold + strings.Join(tokens[0:2], " ") + " " + red + strings.Join(tokens[2:], " ") + reset
-        case (cap(tokens) > 2) && (tokens[1] == "warning:"):
-            cline = bold + strings.Join(tokens[0:2], " ") + " " + yellow + strings.Join(tokens[2:], " ") + reset
-        case (cap(tokens) > 3) && (tokens[1] == "In") && (tokens[2] == "function"):
-            cline = strings.Join(tokens[0:2], " ") + " " + magenta + strings.Join(tokens[3:], " ") + reset
-        case (cap(tokens) > 4) && (tokens[1] == "In") && (tokens[2] == "member") && (tokens[3] == "function"):
-            cline = strings.Join(tokens[0:3], " ") + " " + magenta + strings.Join(tokens[4:], " ") + reset
-    }
+	cline := line
+	switch {
+	case (cap(tokens) > 2) && (tokens[1] == "undefined"):
+		cline = bold + tokens[0] + " " + red + strings.Join(tokens[1:], " ") + reset
+	case (cap(tokens) > 2) && (tokens[1] == "error:"):
+		cline = bold + strings.Join(tokens[0:2], " ") + " " + red + strings.Join(tokens[2:], " ") + reset
+	case (cap(tokens) > 3) && (tokens[1] == "fatal") && (tokens[1] == "error:"):
+		cline = bold + strings.Join(tokens[0:3], " ") + " " + red + strings.Join(tokens[3:], " ") + reset
+	case (cap(tokens) > 2) && (tokens[1] == "warning:"):
+		cline = bold + strings.Join(tokens[0:2], " ") + " " + yellow + strings.Join(tokens[2:], " ") + reset
+	case (cap(tokens) > 3) && (tokens[1] == "In") && (tokens[2] == "function"):
+		cline = strings.Join(tokens[0:2], " ") + " " + magenta + strings.Join(tokens[3:], " ") + reset
+	case (cap(tokens) > 4) && (tokens[1] == "In") && (tokens[2] == "member") && (tokens[3] == "function"):
+		cline = strings.Join(tokens[0:3], " ") + " " + magenta + strings.Join(tokens[4:], " ") + reset
+	case (cap(tokens) > 2) && re1.MatchString(tokens[0]) && (tokens[1] == "***"):
+		cline = bold + red + strings.Join(tokens[:cap(tokens)], " ") + reset
+	}
 
-    return cline
+	return cline
 }
 
 func main() {
